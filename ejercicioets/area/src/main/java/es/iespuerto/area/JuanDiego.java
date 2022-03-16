e00c77e0708027d3e3712596d3cd1a4edcb33c8a
Juan <juaandii13@gmail.com>
2022-03-16 15:08:41 +0000

    Se añade la documentacion inicial del proyecto close #1;

---
+++ ejercicioets/area/src/main/java/es/iespuerto/area/JuanDiego.java
@@ -0,0 +1,53 @@
+package es.iespuerto.area;
+
+/**
+ * Clase Juan Diego
+ *
+ */
+public class JuanDiego 
+{
+    String tipo;
+    Integer valor1;
+    Integer valor2;
+
+    /**
+     * Constructor por defecto de la clase JuanDiego
+     */
+    public JuanDiego() {
+
+    }
+
+    /**
+     * Constructor de la clase JuanDiego que recibe
+     * un parámetro
+     * @param parametro Parametro 1
+     */
+    public JuanDiego(Integer parametro) {
+
+    }
+
+    /**
+     * Constructor de la clase JuanDiego que recibe
+     * dos parámetros
+     * @param parametro1 Parametro 1
+     * @param parametro2 Parametro 2
+     */
+    public JuanDiego(Integer parametro1, Integer parametro2) {
+        
+    }
+
+    /**
+     * Método que recibe como parámetros valor1, y valor2 
+     * para el calculo del área
+     * @param valor1 Valor del primer parámetro para el cálculo del área
+     * @param valor2 Valor del segundo parámetro para el cálculo del área
+     */
+    public void CalculoArea(Integer valor1, Integer valor2) {
+
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola!" );
+    }
+}
---
+++ ejercicioets/area/src/test/java/es/iespuerto/area/AppTest.java
@@ -0,0 +1,20 @@
+package es.iespuerto.area;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
